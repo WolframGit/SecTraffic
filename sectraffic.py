ee4c44323a518df93f81778ebb5f4852f0668c71
Wolfram <gitownering@gmail.com>
2025-08-11 11:48:53 +0300

    sectraffic.py

--- sectraffic.py
+++ sectraffic.py
@@ -6,72 +6,29 @@ from pystyle import Colors, Colorate
 from colorama import Fore, init
 init()
 
-#ЗАХОДИ НА НАШ КАНАЛ!!! (tg: @DecryptageTeam)
-
 #Задаем переменные для уодобного переключения цвета
 BLUE = Fore.LIGHTBLUE_EX
 RED = Fore.LIGHTRED_EX
 RESET = Fore.RESET
 
-def d10m():
-    date_time = dt.now()
-    url = 'https://speedtest.selectel.ru/10MB'
-    print(f' [!] Target activated! - ({date_time.hour}:{date_time.minute}:{date_time.second})')
+def target(ram):
+    try:
+        date_time = dt.now()
+        url = f'https://speedtest.selectel.ru/{ram}'
+        print(f' [!] Target activated! - ({date_time.hour}:{date_time.minute}:{date_time.second})')
     
-    #Вычисление времени и запрос
-    start = t()
-    response = requests.get(url)
-    end = t()
-    fullTime = end - start
+        #Вычисление времени и запрос
+        start = t()
+        response = requests.get(url)
+        end = t()
+        fullTime = end - start
     
-    #Итог по времени выполнения
-    print(f' [~] Время выполнение кода - {fullTime:.0f} sec\n')
-    returner()
-            
-def d100m():
-    date_time = dt.now()
-    url = 'https://speedtest.selectel.ru/100MB'
-    print(f' [!] Target activated! - ({date_time.hour}:{date_time.minute}:{date_time.second})')
-    #Вычисление времени и запрос
-    start = t()
-    response = requests.get(url)
-    end = t()
-    fullTime = end - start
-    
-    #Итог по времени выполнения
-    print(f' [~] Время выполнение кода - {fullTime:.0f} sec\n')
-    returner()
-        
-def d1g():
-    date_time = dt.now()
-    url = 'https://speedtest.selectel.ru/1GB'
-    print(f' [!] Target activated! - ({date_time.hour}:{date_time.minute}:{date_time.second})')
-    #Вычисление времени и запрос
-    start = t()
-    response = requests.get(url)
-    end = t()
-    fullTime = end - start
-    
-    #Итог по времени выполнения
-    print(f' [~] Время выполнение кода - {fullTime:.0f} sec\n')
-    
-    #Конечный вопрос в функции
-    endF = input(' [?] Хотите вернуться обратно? (y/n): ')
-    returner()
-
-def d10g():
-    date_time = dt.now()
-    url = 'https://speedtest.selectel.ru/10GB'
-    print(f' [!] Target activated! - ({date_time.hour}:{date_time.minute}:{date_time.second})')
-    #Вычисление времени и запрос
-    start = t()
-    response = requests.get(url)
-    end = t()
-    fullTime = end - start
-    
-    #Итог по времени выполнения
-    print(f' [~] Время выполнение кода - {fullTime:.0f} sec\n')
-    returner()
+        #Итог по времени выполнения
+        print(f' [~] Время выполнение кода - {fullTime:.0f} sec\n')
+        returner()
+    except KeyboardInterrupt:
+        print(' [!] EXIT!!!')
+        exit
 
 def returner():
     #Конечный вопрос в функции
@@ -97,7 +54,7 @@ def returner():
 
 def main():
     
-    banner = '''
+    banner = r'''
  _______ _______ _______ _______  ______ _______ _______ _______ _____ _______
  |______ |______ |          |    |_____/ |_____| |______ |______   |   |      
  ______| |______ |_____     |    |    \_ |     | |       |       __|__ |_____ 
@@ -109,26 +66,17 @@ def main():
  
  [0] Обо мне / О нас'''
     print(Colorate.Vertical(Colors.red_to_purple, banner, 2))
-    results = input(f'\n{BLUE} [!] Введите опцию -> ')
+    option = ['-', '10MB', '100MB', '1GB', '10GB']
+    int_option = [1, 2, 3, 4]
+    results = int(input(f'\n{BLUE} [!] Введите опцию -> '))
 
-    if results == '1':
-        d10m()
-        
-    elif results == '2':
-        d100m()
-    
-    elif results == '3':
-        d1g()
-
-    elif results == '4':
-        d10g()      
+    if results in int_option:
+        target(option[results])
 
     elif results == '0':
         urlMy = 'https://t.me/DatabaseAttack'
-        urlTeam = 'https://t.me/DecryptageTeam'
         
         webbrowser.open_new(urlMy)
-        webbrowser.open_new(urlTeam)
         
         input(' [!] Нажмите enter, чтобы вернуться в меню...')
         s(2)
